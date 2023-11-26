766a1a7c2e44ec46321bfb89fa4c209e32b22248
Peter Fortune <pfortune@gmail.com>
2023-11-26 01:36:05 +0000

    Added tailwind css

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-white">
+    <div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div class="mx-auto max-w-2xl text-center">
+            <img src="logo.png" alt="Feather Feed Logo" class="mx-auto h-30 w-auto" />
+            <h2 class="mt-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Discover Local Birds.<br />Engage with Feather Feed.
+            </h2>
+            <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">
+                Join our community of bird enthusiasts. Monitor, identify, and learn about the birds visiting your garden.
+            </p>
+        </div>
+    </div>
+</div>
