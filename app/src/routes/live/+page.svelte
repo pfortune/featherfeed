eac3560eb32ae67e2e4db20f04c4b2a8fb7c27c2
Peter Fortune <pfortune@gmail.com>
2023-12-18 10:55:31 +0000

    Adding live bird cam page

---
+++ app/src/routes/live/+page.svelte
@@ -0,0 +1,6 @@
+<iframe
+  title="Live Bird Watching Video Feed"
+  src="https://customer-sm04v2metejg0xy7.cloudflarestream.com/a8b8c1087ef9805cca6f77c322594f63/iframe?muted=true&autoplay=true&poster=https%3A%2F%2Fcustomer-sm04v2metejg0xy7.cloudflarestream.com%2Fa8b8c1087ef9805cca6f77c322594f63%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+  style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
+  allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen;"
+></iframe>
