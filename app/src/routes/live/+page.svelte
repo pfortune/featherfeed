27be2b9aa9e9d6a5ef59df2971275a098e9318f5
Peter Fortune <pfortune@gmail.com>
2023-12-18 12:39:21 +0000

    Updated live stream

--- app/src/routes/live/+page.svelte
+++ app/src/routes/live/+page.svelte
@@ -1,6 +1,14 @@
-<iframe
-  title="Live Bird Watching Video Feed"
-  src="https://customer-sm04v2metejg0xy7.cloudflarestream.com/a8b8c1087ef9805cca6f77c322594f63/iframe?muted=true&autoplay=true&poster=https%3A%2F%2Fcustomer-sm04v2metejg0xy7.cloudflarestream.com%2Fa8b8c1087ef9805cca6f77c322594f63%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
-  style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
-  allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture; fullscreen;"
-></iframe>
+<div class="container mx-auto p-4">
+  <h2 class="text-2xl font-bold mb-4">Live Bird Watching Feed</h2>
+  <div class="video-wrapper" style="position: relative; padding-top: 100%;">
+    <div style="position: relative; padding-top: 56.25%;">
+        <iframe
+          title="Live Bird Watching Feed"
+          src="https://customer-sm04v2metejg0xy7.cloudflarestream.com/c7412d9c564446fe28d78103e094cca9/iframe"
+          style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
+          allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allowfullscreen;fullscreen"
+        ></iframe>
+      </div>
+  </div>
+</div>
+
