b37d708d1415285f80b94b38b612a5ce40e4da31
Peter Fortune <pfortune@gmail.com>
2023-12-30 23:06:12 +0000

    Added detection endpoints

--- api/index.js
+++ api/index.js
@@ -1,13 +1,17 @@
-// Import Hapi framework
+require('dotenv').config();
+import { createClient } from '@supabase/supabase-js'
 const Hapi = require('@hapi/hapi');
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 // Create a new Hapi server with CORS enabled
 const server = Hapi.server({
     port: process.env.PORT || 3000,
     routes: {
         cors: {
-            origin: ['https://featherfeed.ie'], 
-            additionalHeaders: ['cache-control', 'x-requested-with']    
+            origin: ['https://featherfeed.ie'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
         }
     }
 });
@@ -21,6 +25,8 @@ server.route({
             <h1>API Endpoints</h1>
             <ul>
                 <li><a href="/status">/status</a> - Check API status</li>
+                <li><a href="/detections">/detections</a> - Fetch all detections</li>
+                <li>/detections/{id} - Fetch a specific detection by ID</li>
             </ul>
         `;
     }
@@ -35,6 +41,44 @@ server.route({
     }
 });
 
+// Route to fetch all detections
+server.route({
+    method: 'GET',
+    path: '/detections',
+    handler: async (request, h) => {
+        const { data, error } = await supabase
+            .from('detections')
+            .select('*');
+
+        if (error) {
+            console.error('Error fetching detections:', error);
+            return h.response({ error: error.message }).code(500);
+        }
+
+        return data;
+    }
+});
+
+// Route to fetch a specific detection by ID
+server.route({
+    method: 'GET',
+    path: '/detections/{id}',
+    handler: async (request, h) => {
+        const id = request.params.id;
+        const { data, error } = await supabase
+            .from('detections')
+            .select('*')
+            .eq('id', id)
+            .single();
+
+        if (error) {
+            console.error(`Error fetching detection with ID ${id}:`, error);
+            return h.response({ error: error.message }).code(500);
+        }
+
+        return data;
+    }
+});
 
 // Start the server
 const start = async () => {
