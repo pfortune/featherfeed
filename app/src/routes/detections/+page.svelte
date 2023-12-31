be9095e98123e8223f4c14c848cb797a6a87d3b0
Peter Fortune <pfortune@gmail.com>
2023-12-31 18:18:25 +0000

    Updated table with stacked columns for mobile

--- app/src/routes/detections/+page.svelte
+++ app/src/routes/detections/+page.svelte
@@ -45,47 +45,49 @@
 </script>
 
 
-<div class="overflow-x-auto">
-    <table class="min-w-full divide-y divide-gray-200">
+<div class="px-4 mt-10 sm:px-6 lg:px-8">
+    <div class="sm:flex sm:items-center">
+      <div class="sm:flex-auto">
+        <h1 class="text-xl font-semibold leading-6 text-gray-900">Bird Detections</h1>
+        <p class="mt-2 text-sm text-gray-700">A list of all bird detections including date, species, temperature, and humidity.</p>
+      </div>
+    </div>
+    <div class="-mx-4 mt-8 sm:-mx-6 lg:mx-0">
+      <table class="min-w-full divide-y divide-gray-300">
         <thead class="bg-gray-50">
-            <tr>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Date
-                </th>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Species
-                </th>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Temperature
-                </th>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Humidity
-                </th>
-                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    View
-                </th>
-            </tr>
+          <tr>
+            <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Date</th>
+            <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Species</th>
+            <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">Temperature</th>
+            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Humidity</th>
+            <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+              <span class="sr-only">View</span>
+            </th>
+          </tr>
         </thead>
-        <tbody class="bg-white divide-y divide-gray-200">
-            {#each detections as detection}
-                <tr>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {formatDate(detection.date)}
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {detection.species}
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {detection.temperature}°C
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {detection.humidity}%
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                        <button  on:click={() => viewDetails(detection.id)} type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">More Details</button>
-                    </td>                    
-                </tr>
-            {/each}
+        <tbody class="divide-y divide-gray-200 bg-white">
+          {#each detections as detection}
+          <tr>
+            <td class="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">
+              {formatDate(detection.date)}
+              <dl class="font-normal lg:hidden">
+                <dt class="sr-only">Species</dt>
+                <dd class="mt-1 truncate text-gray-700">{detection.species}</dd>
+                <dt class="sr-only">Temperature</dt>
+                <dd class="mt-1 truncate text-gray-500">{detection.temperature}°C</dd>
+                <dt class="sr-only">Humidity</dt>
+                <dd class="mt-1 truncate text-gray-500">{detection.humidity}%</dd>
+              </dl>
+            </td>
+            <td class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{detection.species}</td>
+            <td class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{detection.temperature}°C</td>
+            <td class="px-3 py-4 text-sm text-gray-500">{detection.humidity}%</td>
+            <td class="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                <button  on:click={() => viewDetails(detection.id)} type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">View</button></td>
+          </tr>
+          {/each}
         </tbody>
-    </table>
-</div>
+      </table>
+    </div>
+  </div>
+  
