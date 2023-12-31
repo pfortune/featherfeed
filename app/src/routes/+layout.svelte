57921139c9a6f68b1f9b59a50921f134c06ff826
Peter Fortune <pfortune@gmail.com>
2023-12-31 17:21:23 +0000

    Updated active links

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -45,15 +45,17 @@
       </div>
       <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
         <div class="flex flex-shrink-0 items-center">
-          <img class="h-20 w-auto" src="logo.png" alt="Feather Feed">
+          <a href="/">
+            <img class="h-20 w-auto" src="logo.png" alt="Feather Feed">
+          </a>
         </div>
         <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
           <a href="/" class="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium 
              {($activeRoute === '/') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'}">
-              Dashboard
+              Home
           </a>
           <a href="/detections" class="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium 
-             {($activeRoute === '/detections') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'}">
+             {($activeRoute.startsWith('/detection')) ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'}">
               Detections
           </a>
           <a href="https://github.com/pfortune/FeatherFeed" target="_blank" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Github</a>
@@ -75,7 +77,7 @@
   <div class={isMobileMenuOpen ? '' : 'hidden'} id="mobile-menu">
     <div class="space-y-1 pb-4 pt-2">
       <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
-      <a href="/" class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700">Dashboard</a>
+      <a href="/" class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700">Home</a>
       <a href="/detections" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Detections</a>
       <a href="https://github.com/pfortune/FeatherFeed" target="_blank" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Github</a>
     </div>
