817cfb9b1c55d19640105ebe83dd3c88bf1cf4cb
Peter Fortune <pfortune@gmail.com>
2024-01-03 03:27:41 +0000

    Fixed mobile active page styles

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -62,26 +62,17 @@
       </div>
       
       </div>
-      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <button type="button" class="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-          <span class="absolute -inset-1.5"></span>
-          <span class="sr-only">View notifications</span>
-          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-          </svg>
-        </button>
-      </div>
     </div>
   </div>
 
   <div class={isMobileMenuOpen ? '' : 'hidden'} id="mobile-menu">
     <div class="space-y-1 pb-4 pt-2">
-      <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
-      <a href="/" class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700">Home</a>
-      <a href="/detections" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Detections</a>
+      <a href="/" class="block border-l-4 py-2 pl-3 pr-4 text-base font-medium {($activeRoute === '/') ? 'border-indigo-500 bg-indigo-50 text-indigo-700' : 'border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'}">Home</a>
+      <a href="/detections" class="block border-l-4 py-2 pl-3 pr-4 text-base font-medium {($activeRoute.startsWith('/detection')) ? 'border-indigo-500 bg-indigo-50 text-indigo-700' : 'border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'}">Detections</a>
       <a href="https://github.com/pfortune/FeatherFeed" target="_blank" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Github</a>
     </div>
   </div>
+  
 </nav>
 
 <slot />
