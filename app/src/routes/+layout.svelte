41daade9e323786a17289f4ce4b670b7cbbe3889
Peter Fortune <pfortune@gmail.com>
2024-01-03 22:37:58 +0000

    Fixed logo path

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,78 +1,133 @@
 <!-- Assuming this is in your layout file -->
 <script>
-  import { page } from '$app/stores';
-  import { derived } from 'svelte/store';
+	import { page } from '$app/stores';
+	import { derived } from 'svelte/store';
 
-  let isMobileMenuOpen = false;
+	let isMobileMenuOpen = false;
 
-  function toggleMobileMenu() {
-      isMobileMenuOpen = !isMobileMenuOpen;
-  }
+	function toggleMobileMenu() {
+		isMobileMenuOpen = !isMobileMenuOpen;
+	}
 
-  const activeRoute = derived(page, $page => $page.url.pathname);
+	const activeRoute = derived(page, ($page) => $page.url.pathname);
 
-  import "../app.css";
+	import '../app.css';
 </script>
 
 <nav class="bg-white shadow">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div class="relative flex h-16 justify-between">
-      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        <!-- Mobile menu button -->
-        <button on:click={toggleMobileMenu} type="button" class="relative inline-flex items-center justify-center 
-        rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 
-        focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" 
-        aria-controls="mobile-menu" aria-expanded="false">
-          <span class="absolute -inset-0.5"></span>
-          <span class="sr-only">Open main menu</span>
-          <!--
+	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+		<div class="relative flex h-16 justify-between">
+			<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+				<!-- Mobile menu button -->
+				<button
+					on:click={toggleMobileMenu}
+					type="button"
+					class="relative inline-flex items-center justify-center
+        rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500
+        focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+					aria-controls="mobile-menu"
+					aria-expanded="false"
+				>
+					<span class="absolute -inset-0.5"></span>
+					<span class="sr-only">Open main menu</span>
+					<!--
             Icon when menu is closed.
 
             Menu open: "hidden", Menu closed: "block"
           -->
-          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          <!--
+					<svg
+						class="block h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+						/>
+					</svg>
+					<!--
             Icon when menu is open.
 
             Menu open: "block", Menu closed: "hidden"
           -->
-          <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div class="flex flex-shrink-0 items-center">
-          <a href="/">
-            <img class="h-20 w-auto" src="logo.png" alt="Feather Feed">
-          </a>
-        </div>
-        <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
-          <a href="/" class="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium 
-             {($activeRoute === '/') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'}">
-              Home
-          </a>
-          <a href="/detections" class="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium 
-             {($activeRoute.startsWith('/detection')) ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'}">
-              Detections
-          </a>
-          <a href="https://github.com/pfortune/FeatherFeed" target="_blank" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Github</a>
-      </div>
-      
-      </div>
-    </div>
-  </div>
+					<svg
+						class="hidden h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</button>
+			</div>
+			<div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+				<div class="flex flex-shrink-0 items-center">
+					<a href="/">
+						<img class="h-20 w-auto" src="/logo.png" alt="Feather Feed" />
+					</a>
+				</div>
+				<div class="hidden sm:ml-6 sm:flex sm:space-x-8">
+					<a
+						href="/"
+						class="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium
+             {$activeRoute === '/'
+							? 'border-indigo-500 text-gray-900'
+							: 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'}"
+					>
+						Home
+					</a>
+					<a
+						href="/detections"
+						class="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium
+             {$activeRoute.startsWith('/detection')
+							? 'border-indigo-500 text-gray-900'
+							: 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'}"
+					>
+						Detections
+					</a>
+					<a
+						href="https://github.com/pfortune/FeatherFeed"
+						target="_blank"
+						class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+						>Github</a
+					>
+				</div>
+			</div>
+		</div>
+	</div>
 
-  <div class={isMobileMenuOpen ? '' : 'hidden'} id="mobile-menu">
-    <div class="space-y-1 pb-4 pt-2">
-      <a href="/" class="block border-l-4 py-2 pl-3 pr-4 text-base font-medium {($activeRoute === '/') ? 'border-indigo-500 bg-indigo-50 text-indigo-700' : 'border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'}">Home</a>
-      <a href="/detections" class="block border-l-4 py-2 pl-3 pr-4 text-base font-medium {($activeRoute.startsWith('/detection')) ? 'border-indigo-500 bg-indigo-50 text-indigo-700' : 'border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'}">Detections</a>
-      <a href="https://github.com/pfortune/FeatherFeed" target="_blank" class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Github</a>
-    </div>
-  </div>
-  
+	<div class={isMobileMenuOpen ? '' : 'hidden'} id="mobile-menu">
+		<div class="space-y-1 pb-4 pt-2">
+			<a
+				href="/"
+				class="block border-l-4 py-2 pl-3 pr-4 text-base font-medium {$activeRoute === '/'
+					? 'border-indigo-500 bg-indigo-50 text-indigo-700'
+					: 'border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'}"
+				>Home</a
+			>
+			<a
+				href="/detections"
+				class="block border-l-4 py-2 pl-3 pr-4 text-base font-medium {$activeRoute.startsWith(
+					'/detection'
+				)
+					? 'border-indigo-500 bg-indigo-50 text-indigo-700'
+					: 'border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'}"
+				>Detections</a
+			>
+			<a
+				href="https://github.com/pfortune/FeatherFeed"
+				target="_blank"
+				class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
+				>Github</a
+			>
+		</div>
+	</div>
 </nav>
 
 <slot />
